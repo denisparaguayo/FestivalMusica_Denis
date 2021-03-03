1eb3d3e7d38d6824d27d9307ddd640d466e66d80
Denis Ricardo <denis.cantero1987@gmail.com>
2021-03-02 22:59:12 -0300

    nuevas funciones de sass y agregando line up

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,53 @@
 const { series, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
 
 
+const paths = {
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss'
+}
 //Función que compila sass
 
+
+
+
 function css() {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         .pipe(sass())
         .pipe(dest('./build/css'))
 }
 
 function minificarcss() {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(dest('./build/css'))
 }
 
+function imagenes(){
+    return src(paths.imagenes)
+    .pipe(imagemin())
+    .pipe(dest('./build/img'))
+    .pipe(notify({message: 'imagen Minificada'}))
+}
+
+function versionwebp(){
+    return src(paths.imagenes)
+    .pipe(webp())
+    .pipe(dest('./build/img'))
+    .pipe(notify({message: 'Version WebP Listo'}))
+    
+}
+
 function watchArchivos() {
-    watch('src/scss/**/*.scss', css);
+    watch(paths.scss, css);
 
 }
 
 exports.css = css;
 exports.minificarcss = minificarcss;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
+exports.default = series(css, imagenes, versionwebp, watchArchivos)
