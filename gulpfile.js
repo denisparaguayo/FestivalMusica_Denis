9b66512d75f0f7306f71bdd4a129a13a352dcfc8
Denis Ricardo <denis.cantero1987@gmail.com>
2021-03-06 13:19:50 -0300

    galeria

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
-const { series, src, dest, watch } = require('gulp');
+const { series, src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
 const notify = require('gulp-notify');
 const webp = require('gulp-webp');
+const concat = require('gulp-concat');
+
 
 
 const paths = {
     imagenes: 'src/img/**/*',
-    scss: 'src/scss/**/*.scss'
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js'
 }
 //Función que compila sass
 
@@ -24,6 +27,12 @@ function minificarcss() {
     return src(paths.scss)
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(dest('./build/css'))
+}
+
+function javascript(){
+    return src(paths.js)
+    .pipe(concat('bundle.js'))
+    .pipe(dest('./build/js'))
 }
 
 function imagenes(){
@@ -43,6 +52,8 @@ function versionwebp(){
 
 function watchArchivos() {
     watch(paths.scss, css);
+    watch(paths.js, javascript );
+    
 
 }
 
@@ -50,4 +61,4 @@ exports.css = css;
 exports.minificarcss = minificarcss;
 exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
-exports.default = series(css, imagenes, versionwebp, watchArchivos)
+exports.default = series(css, javascript, imagenes, versionwebp, watchArchivos)
